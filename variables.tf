ebffbfe4cf9155e01fc4d3fec1d6478418fe2c78
alparslanu6347 <alparslanu6347@gmail.com>
2025-01-06 18:24:00 +0100

    should define your required variables

--- variables.tf
+++ variables.tf
@@ -1,18 +1,15 @@
 variable "key_name" {    // !!! This is a required input !!!
   type        = string
-#   default = "arrow"
   description = "The name of the SSH key pair used to access EC2 instances. This is a required input, and there is no default value."
 }
 
 variable "subnet" {     // !!! This is a required input !!!
   type        = string
-#   default     = "subnet-c41ba589" 
   description = "The subnet ID for deploying resources in the Availability Zone (example = us-east-1a) you chose. Ensure instances are in the same AZ to minimize inter-AZ data transfer costs. This is a required input, and there is no default value."
 }
 
 variable "azone" {      // !!! This is a required input !!!
   type        = string
-#   default     = "us-east-1a"
   description = "The Availability Zone (AZ) where resources will be deployed. Choose the AZ that best suits your requirements. This is a required input, and there is no default value."
 }
 
