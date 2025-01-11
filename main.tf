21a1a6d7c663f17fe7ef97145f86ac94df1f77bc
alparslanu6347 <alparslanu6347@gmail.com>
2025-01-11 21:06:24 +0100

    main.tf updated

--- main.tf
+++ main.tf
@@ -24,8 +24,8 @@ resource "aws_iam_instance_profile" "ec2connectprofile" {
 resource "aws_iam_role" "ec2connectcli" {
   name               = "ec2connectcli-${local.name}"
   assume_role_policy = jsonencode({
-    Version          = "2012-10-17"
-    Statement        = [
+    Version = "2012-10-17"
+    Statement = [
       {
         Action    = "sts:AssumeRole"
         Effect    = "Allow"
@@ -36,31 +36,31 @@ resource "aws_iam_role" "ec2connectcli" {
       },
     ]
   })
+}
 
-  inline_policy {
-    name = "my_inline_policy"
-
-    policy      = jsonencode({
-      Version   = "2012-10-17"
-      Statement = [
-        {
-          "Effect" : "Allow",
-          "Action" : "ec2-instance-connect:SendSSHPublicKey",
-          "Resource" : "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:instance/*",
-          "Condition" : {
-            "StringEquals" : {
-              "ec2:osuser" : "ubuntu"
-            }
+resource "aws_iam_role_policy" "my_inline_policy" {
+  name   = "my_inline_policy"
+  role   = aws_iam_role.ec2connectcli.id
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Action" : "ec2-instance-connect:SendSSHPublicKey",
+        "Resource" : "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:instance/*",
+        "Condition" : {
+          "StringEquals" : {
+            "ec2:osuser" : "ubuntu"
           }
-        },
-        {
-          "Effect" : "Allow",
-          "Action" : "ec2:DescribeInstances",
-          "Resource" : "*"
         }
-      ]
-    })
-  }
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : "ec2:DescribeInstances",
+        "Resource" : "*"
+      }
+    ]
+  })
 }
 
 
